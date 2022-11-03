181d4b10dbf723896524318d9f943bbab75bcb4b
PRamsey02 <prestonkramsey02@icloud.com>
2022-11-03 12:42:27 -0500

    Defined columns in ProductTag.js and Tag.js

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -6,15 +6,34 @@ class ProductTag extends Model {}
 
 ProductTag.init(
   {
-    // define columns
+   // define columns
+   id: {
+    type: DataTypes.INTEGER, 
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true
   },
-  {
-    sequelize,
-    timestamps: false,
-    freezeTableName: true,
-    underscored: true,
-    modelName: 'product_tag',
+  product_id: {
+    type: DataTypes.INTEGER,
+    references: {
+      model: 'product',
+      key: 'id'
+    }
+  },
+  tag_id: {
+    type: DataTypes.INTEGER,
+    references: {
+      model: 'tag',
+      key: 'id'
+    }
   }
+},
+{
+  sequelize,
+  timestamps: false,
+  freezeTableName: true,
+  underscored: true,
+  modelName: 'product_tag',
+}
 );
-
 module.exports = ProductTag;
